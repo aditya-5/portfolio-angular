bf2c9331ed301b37b344bc4401e2f2dde59fec64
Aditya Agarwal <aditya.7@outlook.com>
2021-06-27 16:39:40 +0530

    Child education component

---
+++ src/app/education-desc/education-desc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-education-desc',
+  templateUrl: './education-desc.component.html',
+  styleUrls: ['./education-desc.component.css']
+})
+export class EducationDescComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
