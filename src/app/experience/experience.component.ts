b30e3a3b19be116cd73308c723b4bcd0588d1722
Aditya Agarwal <aditya.7@outlook.com>
2021-06-28 22:40:40 +0530

    Added the experience component

---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
+})
+export class ExperienceComponent implements OnInit {
+
+  experienceType: boolean[] = [true, false, false];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeExpType(n : number){
+    for(let i=0;i<this.experienceType.length;i++){
+      this.experienceType[i] = !this.experienceType;
+    }
+
+    this.experienceType[n] = true
+  }
+
+}
